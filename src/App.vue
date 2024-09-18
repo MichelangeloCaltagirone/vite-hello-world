ecf8cdb6d62516f3dcb8bfa67df72147aa720962
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-18 17:55:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+
+
+<script>
+
+import AppTitle from './components/AppTitle.vue';
+  
+import AppHero from './components/AppHero.vue';
+
+
+  export default {
+    components:{
+      AppTitle,
+      AppHero
+    },
+    data() {
+      return {
+      }
+    }
+  } 
+
+</script>
+
+<template>
+
+  <div>
+    <AppTitle />
+    <AppHero />
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <p>Ciao, sarò un jumbotron</p>
+</template>
+
+<style>
+</style>
